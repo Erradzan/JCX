8119e76a3f593f58754f3dff3b59dc370ad0056e
Erlangga <erradzan@gmail.com>
2024-07-03 13:34:11 +0700

    modul 2 final assignment

--- Script.js
+++ Script.js
@@ -2,3 +2,31 @@ document.getElementById('hamburger').addEventListener('click', function () {
     this.classList.toggle('open');
     document.getElementById('menu').classList.toggle('open');
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const modeToggle = document.getElementById('darkModeToggle');
+    const logo = document.getElementById('logo');
+    const favicon = document.getElementById('icon');
+    const currentMode = localStorage.getItem('mode') || 'light';
+    setMode(currentMode);
+
+    modeToggle.addEventListener('click', () => {
+        const newMode = document.body.classList.contains('dark-mode') ? 'light' : 'dark';
+        setMode(newMode);
+    });
+
+    function setMode(mode) {
+        if (mode === 'dark') {
+            document.body.classList.add('dark-mode');
+            modeToggle.textContent = 'Light Mode';
+            logo.src = 'Support/Dark Logo.png';
+            favicon.href = 'Support/Dark Icon.png';
+        } else {
+            document.body.classList.remove('dark-mode');
+            modeToggle.textContent = 'Dark Mode';
+            logo.src = 'Support/Light Logo.png';
+            favicon.href = 'Support/Light Icon.png';
+        }
+        localStorage.setItem('mode', mode);
+    }
+});
